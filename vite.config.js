7e3b4b9bba90ad4306895fabd74c6dc3bf3400bc
Nicolas MASSART <nicolas.massart@consensys.net>
2026-04-09 16:26:29 +0200

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  // Must match the GitHub Pages repository sub-path
+  base: '/cfp-sim-couts/',
   plugins: [react()],
   build: {
     outDir: 'dist',
